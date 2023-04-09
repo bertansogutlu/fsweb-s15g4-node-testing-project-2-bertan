893b67b5c9eb0d997f9b748b33e2738ae57b2afc
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-09 21:32:03 +0300

    gorev modelleri olusturuldu

--- api/gorevler/gorev-model.js
+++ api/gorevler/gorev-model.js
@@ -1,1 +1,33 @@
-const db = require('../../data/db-config')
+const db = require('../../data/db-config');
+
+async function getAll () {
+    return await db('Gorevler');
+}
+
+async function getById (id) {
+    return await db('Gorevler').where('GorevID',id).first();
+}
+
+async function create (gorev) {
+    const [id] = await db('Gorevler').insert(gorev);
+    const yeniGorev = await getById(id);
+    return yeniGorev;
+}
+
+async function update (id,gorev) {
+    await db('Gorevler').where('GorevID',id).update(gorev);
+    const guncelGorev = await getById(id);
+    return guncelGorev;
+}
+
+async function remove (id) {
+    return db('Gorevler').where('GorevID',id).del();
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
