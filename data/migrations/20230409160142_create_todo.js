a13677aa19e2b6ecebc62204fe0578619accecee
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-09 19:14:47 +0300

    migration schema olusturuldu

--- data/migrations/20230409160142_create_todo.js
+++ data/migrations/20230409160142_create_todo.js
@@ -3,7 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  const all = knex.schema.createTable("Gorevler",(table)=>{
+    table.increments("GorevID")
+    table.string("Adi").notNullable
+    table.string("Aciklama").notNullable
+  })
+  .createTable("Tasklar",(table)=>{
+    table.increments("TaskID")
+    table.string("Adi").notNullable
+    table.string("Aciklama").notNullable
+    table.integer("GorevID").references("GorevID").inTable("Gorevler").onDelete("CASCADE").onUpdate("CASCADE")
+  })
+  return all;
 };
 
 /**
@@ -11,5 +22,7 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists("Tasklar")
+  .dropTableIfExists("Gorevler")
 };
