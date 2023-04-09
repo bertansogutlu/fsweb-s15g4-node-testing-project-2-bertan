e0927d2f72056a9e9d5a646a21b0bb2fd328b021
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-09 19:00:01 +0300

    Ana dosyalar olusturuldu

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+const shareConfig = {
+
+  client: 'sqlite3',
+
+  migrations: {
+    directory: "./data/migrations"
+  },
+  seeds: {
+    directory: "./data/seeds"
+  },
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn,donn)=>{
+      conn.run("PRAGMA foreign_keys = ON",done)
+    }
+  }
+}
+
+module.exports = {
+
+  development: {
+    ...shareConfig,
+    connection: {
+      filename: './todo-app.sqlite3'
+    },
+  },
+  testing: {
+    ...shareConfig,
+    connection: {
+      filename: './todo-app-testing.sqlite3'
+    },
+  }
+
+};
